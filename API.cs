4e3ffb6a5bf06a6d87fbec7e6e170f9d6019c406
EarsKilla <mne.pipirku@otso.city>
2020-09-11 00:13:37 +0300

    customizable source

--- API.cs
+++ API.cs
@@ -19,19 +19,19 @@ namespace GetContactAPI
         /// <summary>
         /// Возвращает основную информацию по номеру телефону
         /// </summary>
-        public ApiResponse<SearchResult> GetByPhone(string phone, string countryCode = null)
+        public ApiResponse<SearchResult> GetByPhone(string phone, string countryCode = null, string source = null)
         {
             if (String.IsNullOrEmpty(phone) || !Regex.IsMatch(phone, "\\+?\\d{11}")) throw new ArgumentException("Телефон заполнен неправильно");
-            return _topic.CreateTopic<SearchResult>("https://pbssrv-centralevents.com/v2.5/search", "search", phone, countryCode);
+            return _topic.CreateTopic<SearchResult>("https://pbssrv-centralevents.com/v2.5/search", source ?? "search", phone, countryCode);
         }
 
         /// <summary>
         /// Возвращает список тегов
         /// </summary>
-        public ApiResponse<DetailsResult> GetTags(string phone, string countryCode = null)
+        public ApiResponse<DetailsResult> GetTags(string phone, string countryCode = null, string source = null)
         {
             if (String.IsNullOrEmpty(phone) || !Regex.IsMatch(phone, "\\+?\\d{11}")) throw new ArgumentException("Телефон заполнен неправильно");
-            return _topic.CreateTopic<DetailsResult>("https://pbssrv-centralevents.com/v2.5/number-detail", "details", phone, countryCode);
+            return _topic.CreateTopic<DetailsResult>("https://pbssrv-centralevents.com/v2.5/number-detail", source ?? "details", phone, countryCode);
         }
     }
 }
