e740f8ffd88dd189fee512e4d862166981281009
EarsKilla <mne.pipirku@otso.city>
2020-09-11 16:02:46 +0300

    force option to skip phone number checks (not recommended)

--- API.cs
+++ API.cs
@@ -9,6 +9,8 @@ namespace GetContactAPI
 {
     public class API
     {
+        private static readonly Regex PhoneMatcher = new Regex(@"\+?\d{11}");
+        
         private readonly Topic _topic;
 
         public API(ConfigBuilder.Config config)
@@ -19,18 +21,18 @@ namespace GetContactAPI
         /// <summary>
         /// Возвращает основную информацию по номеру телефону
         /// </summary>
-        public ApiResponse<SearchResult> GetByPhone(string phone, string countryCode = null, string source = null)
+        public ApiResponse<SearchResult> GetByPhone(string phone, string countryCode = null, string source = null, bool force = false)
         {
-            if (String.IsNullOrEmpty(phone) || !Regex.IsMatch(phone, "\\+?\\d{11}")) throw new ArgumentException("Телефон заполнен неправильно");
+            if (!force && (string.IsNullOrEmpty(phone) || !PhoneMatcher.IsMatch(phone))) throw new ArgumentException("Телефон заполнен неправильно");
             return _topic.CreateTopic<SearchResult>("https://pbssrv-centralevents.com/v2.5/search", source ?? "search", phone, countryCode);
         }
 
         /// <summary>
         /// Возвращает список тегов
         /// </summary>
-        public ApiResponse<DetailsResult> GetTags(string phone, string countryCode = null, string source = null)
+        public ApiResponse<DetailsResult> GetTags(string phone, string countryCode = null, string source = null, bool force = false)
         {
-            if (String.IsNullOrEmpty(phone) || !Regex.IsMatch(phone, "\\+?\\d{11}")) throw new ArgumentException("Телефон заполнен неправильно");
+            if (!force && (string.IsNullOrEmpty(phone) || !PhoneMatcher.IsMatch(phone))) throw new ArgumentException("Телефон заполнен неправильно");
             return _topic.CreateTopic<DetailsResult>("https://pbssrv-centralevents.com/v2.5/number-detail", source ?? "details", phone, countryCode);
         }
     }
